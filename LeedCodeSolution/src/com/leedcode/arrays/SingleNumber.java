3263a69aac37b9520eff5bbd0673ae7118c9b0b4
Gokilavani06 <>
2026-07-16 15:19:02 +0530

    7. Single Number
    Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
    
    You must implement a solution with a linear runtime complexity and use only constant extra space.
    
    Example 1:
    
    Input: nums = [2,2,1]
    
    Output: 1
    
    Example 2:
    
    Input: nums = [4,1,2,1,2]
    
    Output: 4
    
    Example 3:
    
    Input: nums = [1]
    
    Output: 1
    
    Constraints:
    
    1 <= nums.length <= 3 * 104
    -3 * 104 <= nums[i] <= 3 * 104
    Each element in the array appears twice except for one element which appears only once.
    
    Signed-off-by: Gokilavani06 <>

---
+++ LeedCodeSolution/src/com/leedcode/arrays/SingleNumber.java
@@ -0,0 +1,15 @@
+package com.leedcode.arrays;
+
+
+class SingleNumber {
+    public int singleNumber(int[] nums) {
+
+        int result=0;
+        for(int i=0;i<nums.length;i++){
+            result=result^nums[i];
+        }
+
+        return result;
+        
+    }
+}
