1092035a98401975625fc7b37c5c0003f2cb85c1
Gokilavani06 <123656847+Gokilavani06@users.noreply.github.com>
2026-07-22 18:50:13 +0530

    16. Missing Numbers
    Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
    
     
    
    Example 1:
    
    Input: nums = [3,0,1]
    
    Output: 2
    
    Explanation:
    
    n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
    
    Example 2:
    
    Input: nums = [0,1]
    
    Output: 2
    
    Explanation:
    
    n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
    
    Example 3:
    
    Input: nums = [9,6,4,2,3,5,7,0,1]
    
    Output: 8
    
    Explanation:
    
    n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
    
     
     
    
     
    
     
    
    Constraints:
    
    n == nums.length
    1 <= n <= 104
    0 <= nums[i] <= n
    All the numbers of nums are unique.

---
+++ LeedCodeSolution/src/com/leedcode/arrays/MissingNumbers.java
@@ -0,0 +1,18 @@
+package com.leedcode.arrays;
+
+import java.util.Arrays;
+
+public class MissingNumbers {
+	    public int missingNumber(int[] nums) {
+
+	        Arrays.sort(nums);
+	        for (int i = 0; i < nums.length; i++) {
+	            if (i != nums[i]) {
+	                return i;
+	            }
+
+	        }
+	        return nums[nums.length - 1] + 1;
+
+	}
+}
